584ffe616389dc70f6760bb436a6ad2484fdc96a
Kevin Le <kevinle29299@gmail.com>
2026-02-25 14:10:53 -0800

    Implement javascript main functionality of the app

---
+++ script.js
@@ -0,0 +1,21 @@
+//Declare variables
+let counter = 0
+
+//Get elements that update (counter and previous entries)
+let ctrTxt = document.getElementById("counterTxt")
+let prevEntr = document.getElementById("prevEntries")
+
+function increment(){
+    counter += 1;
+    ctrTxt.innerText = counter;
+}
+
+function save(){
+    //Update previous counter txt
+    let prevTxt = counter + " - "
+    prevEntr.textContent += prevTxt
+
+    //After saving, reset counter text and counter var
+    ctrTxt.innerText = 0
+    counter = 0
+}
